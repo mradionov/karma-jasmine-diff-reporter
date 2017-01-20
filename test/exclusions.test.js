f269de3cd2c8ca6064209e409317c2da38647dab
Michael Radionov <micrddrgn@gmail.com>
2017-01-20 22:13:48 +0300

    refactor: Ignore 'not' keyword. Clear result message from marker.

--- test/exclusions.test.js
+++ test/exclusions.test.js
@@ -8,3 +8,12 @@ test('unmatched message',
 
   `Some random text`
 );
+
+// Note: this behavior is a bit weird comparing to toBeUndefined and others -
+// that equal parts are highlighted with default colors. Not critical
+test('no diff for equal numbers',
+
+  `Expected 5 not to be 5.`,
+
+  `Expected 5 not to be 5.`
+);
--- test/stack-trace.test.js
+++ test/stack-trace.test.js
@@ -13,21 +13,3 @@ test('don\'t capture stacktrace without "at"',
 
   { stack: `\ncheck@/path/to/file.js:42:0` }
 );
-
-// Note: this behavior is a bit weird comparing to toBeUndefined and others -
-// that equal parts are highlighted with default colors. Not critical
-// test('toEqual: no diff for equal objects', function (assert) {
-//   var input =
-//     "Expected Object({ foo: 'bar' }) not to equal Object({ foo: 'bar' })." +
-//     stack;
-//   var expected =
-//     "Expected <d>Object({ foo: 'bar' })</d>" +
-//     " not to equal <d>Object({ foo: 'bar' })</d>." +
-//     stack;
-
-//   var out = diff(input, formatter);
-
-//   assert.equal(out, expected);
-//   assert.end();
-// });
-//
